a7431868fa25cb14158b266807c638e94d6a9159
KolobokMysnoy <inkasator200@gmain.com>
2025-02-22 19:29:46 +0300

    Fifth day

---
+++ fifth/second/def.go
@@ -0,0 +1,45 @@
+package main
+
+func abs(n int) int {
+	if n < 0 {
+		return -n
+	}
+
+	return n
+}
+
+func partition(arr []int, low, high int) ([]int, int) {
+	pivot := arr[high]
+	i := low
+	for j := low; j < high; j++ {
+		if arr[j] < pivot {
+			arr[i], arr[j] = arr[j], arr[i]
+			i++
+		}
+	}
+	arr[i], arr[high] = arr[high], arr[i]
+	return arr, i
+}
+
+func quickSort(arr []int, low, high int) []int {
+	if low < high {
+		var p int
+		arr, p = partition(arr, low, high)
+		arr = quickSort(arr, low, p-1)
+		arr = quickSort(arr, p+1, high)
+	}
+	return arr
+}
+
+func quickSortStart(arr []int) []int {
+	return quickSort(arr, 0, len(arr)-1)
+}
+
+func findElement(arr []int, target int) bool {
+	for _, value := range arr {
+		if value == target {
+			return true
+		}
+	}
+	return false
+}
